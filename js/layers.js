6809fe8092da17c822a3247e9de158bff038a630
Douglas <Darkcoalpimin@gmail.com>
2022-08-13 15:42:12 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -35,6 +35,11 @@ addLayer("p", {
             title: "Make this whatever you want!",
             description: "Double your point gain.",
             cost: new Decimal(2)
+        },
+        12: {
+            title: "Make this whatever you want!",
+            description: "Double your point gain.",
+            cost: new Decimal(3)
         }
     },
 })
