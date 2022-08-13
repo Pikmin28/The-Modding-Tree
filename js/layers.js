a36519675c4282b99daf1efb55d4ccb179cc068b
Douglas <Darkcoalpimin@gmail.com>
2022-08-13 15:34:35 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -35,11 +35,7 @@ addLayer("p", {
             title: "Make this whatever you want!",
             description: "Double your point gain.",
             cost: new Decimal(2)
-        },
-        effect() {
-            return player[this.layer].points.add(1).pow(0.5)
-        },
-        effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        }
     }
 })
 
