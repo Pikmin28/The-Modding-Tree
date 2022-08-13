a33e1132f25e7ca84f42a30ed6b60e234f371e9a
Douglas <Darkcoalpimin@gmail.com>
2022-08-13 14:21:38 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -22,7 +22,7 @@ addLayer("rp", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Lose you pellets to grow red pikmin", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
