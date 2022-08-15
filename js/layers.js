cffe60f594b6554357ababe5a5795052c99ca3c3
Douglas <DarkCoalPikmin@Gmail.com>
2022-08-14 22:24:03 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -70,7 +70,7 @@ addLayer("q", {
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "red pikmin2", // Name of prestige currency
     baseResource: "red pikmin", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.pikmin}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
