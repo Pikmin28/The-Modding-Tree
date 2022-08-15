36a43539adbf654359f04b4e91e73b4cf4003851
Douglas <DarkCoalPikmin@Gmail.com>
2022-08-14 22:17:35 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -58,4 +58,31 @@ addLayer("p", {
         }
     }
 })
-
+addLayer("q", {
+    name: "grow red pikmin2", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "RE", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0), // Starting amount
+    }},
+    color: "#4BDC12",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "red pikmin2", // Name of prestige currency
+    baseResource: "pellets2", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "q", description: "P: Lose you pellets to grow red pikmin", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
