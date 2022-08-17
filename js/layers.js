df5b6b88cca52064c6640e89d61937e5eb952651
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-17 13:01:55 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -155,7 +155,7 @@ addLayer("x", {
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+        return new Decimal(.1)
     },
     row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
