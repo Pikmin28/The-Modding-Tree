018daf3677c4ed8423f8b4e78ccbfe6adcd9399c
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-17 22:24:05 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -86,6 +86,7 @@ addLayer("q", {
     baseResource: "red pikmin", // Name of resource prestige is based on
     baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    canBuyMax: true,
     exponent: .5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
