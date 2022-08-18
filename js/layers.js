31b52c2782e01d9168c4730eaa4dc9a4cb986c5f
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-18 14:52:35 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -170,9 +170,9 @@ addLayer("x", {
     ],
     layerShown(){return true},
     effect() {
-        return player[this.layer].points.add(1).pow(0.33)
-    },
-    effectDisplay() { return format(upgradeEffect(this.layer))+"x" },
+        return {  cat: player[this.layer].points.add(1).pow(0.33)
+    }},
+    effectDisplay() { return format(cat)+"x" },
     upgrades: {
         11: {
             title: "Send your red pikmin to collect pellets",
