cb7d93832528fd6f79906e61a271e183f270fb69
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-18 22:57:48 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -88,7 +88,7 @@ addLayer("q", {
     baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     canBuyMax: true,
-    exponent: .5, // Prestige currency exponent
+    exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
