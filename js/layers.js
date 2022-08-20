62d272b01a38de3c386c7146caf2812ff916f9ca
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-19 22:29:42 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -231,7 +231,7 @@ addLayer("x", {
         requires: new Decimal(315), // Can be a function that takes requirement increases into account
         resource: "Red Buds", // Name of prestige currency
         baseResource: "red pikmin", // Name of resource prestige is based on
-        baseAmount() {return player.p.points}, // Get the current amount of baseResource
+        baseAmount() {return player.points}, // Get the current amount of baseResource
         type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
         exponent: 1.25, // Prestige currency exponent
         gainMult() { // Calculate the multiplier for main currency from bonuses
