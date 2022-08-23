8563beb0b132145f4117b447a7a792228993a104
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-22 20:23:49 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -258,7 +258,7 @@ addLayer("x", {
         }},
         effectDescription() { // Optional text to describe the effects
             eff = this.effect();
-            return "which are boosting buds by "+format(eff.cat)
+            return "which is boosting buds by "+format(eff.cat) + "which are boosting self by "+format(eff.mat)
         },
         upgrades: {
             11: {
